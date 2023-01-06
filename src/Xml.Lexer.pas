a149c7357e92d2ddaa29695e52212595cd9e3601
lmbelo <lucas.belo@live.com>
2023-01-06 12:46:48 -0300

    Revert "Using an index array only as the state machine"
    This reverts commit 75f470cb7a9927ac3107db67f1787154766b59fb.

--- src/Xml.Lexer.pas
+++ src/Xml.Lexer.pas
@@ -1,18 +1,10 @@
-(********************************************************
- *                XML Lexer for Delphi                  *
- *                                                      *
- * Copyright (c) 2023 by Lucas Moura Belo - lmbelo      *
- * Licensed under the MIT License                       *
- *                                                      *
- * For full license text and more information visit:    *
- * https://github.com/lmbelo/xml-lexer4delphi           *
- ********************************************************)
 unit Xml.Lexer;
 
 interface
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  System.Generics.Collections;
 
 type
   {$SCOPEDENUMS ON}
@@ -47,17 +39,25 @@ type
     attributeName,
     attributeValue
   );
+
+  TActionAndHandler = record
+    Action: TAction;
+    Handler: TProc<char>;
+  end;
   {$SCOPEDENUMS OFF}
 
   TLexer = class
-  public type
-    THandler = TProc<char>;
-    TEvent = reference to procedure(const AType: TType; const AData: string);
   private type
-    TStateMachineActionHandler = array[TAction.lt..TAction.error] of THandler;
-    TStateMachine = array[TState.data..TState.attributeValue] of TStateMachineActionHandler;
+    TStaticStateMachineActionHandler = array[TAction.lt..TAction.error] of TProc<char>;
+    TStaticStateMachine = array[TState.data..TState.attributeValue] of TStaticStateMachineActionHandler;
+  public type
+    TActionHandler = TProc<char>;
+    TEvent = reference to procedure(const AType: TType; const AData: string);
+    TActionPair = TPair<TAction, TActionHandler>;
+    TStateActions = TDictionary<TAction, TActionHandler>;
+    TStateMachine = TObjectDictionary<TState, TStateActions>;
   private
-    FStateMachine: TStateMachine;
+    FStaticStateMachine: TStaticStateMachine;
   private
     FEvent: TEvent;
     FState: TState;
@@ -67,18 +67,21 @@ type
     FAttrValue: string;
     FIsClosing: boolean;
     FOpeningQuote: string;
+    FStateMachine: TStateMachine;
     procedure Emit(const AType: TType; const AData: string); inline;
   protected
     procedure CreateStateMachine(); virtual;
     procedure Step(const AChar: char); inline;
-  public const
-    STATE_ACTION_UNREACHABLE = nil;
   public
     constructor Create(const AEvent: TEvent);
+    destructor Destroy(); override;
 
     /// <summary>
+    ///    Build the static state machine with the latest actions.
+    /// </summary>
+    procedure Build();
+    /// <summary>
     ///    Tokenize the characters.
-    ///    It supports chuncks of data.
     /// </summary>
     procedure Write(const AXmlData: string);
 
@@ -102,7 +105,15 @@ begin
   inherited Create();
   FEvent := AEvent;
   FState := TState.data;
+  FStateMachine := TStateMachine.Create([doOwnsValues]);  
   CreateStateMachine();
+  Build();
+end;
+
+destructor TLexer.Destroy;
+begin
+  FStateMachine.Free();
+  inherited;
 end;
 
 procedure TLexer.Emit(const AType: TType; const AData: string);
@@ -133,15 +144,25 @@ begin
     else  LAction := TAction.char;
   end;
 
-  //Convert a char to a default action (or char action)
-  if (FStateMachine[FState][LAction] <> STATE_ACTION_UNREACHABLE) then
-    FStateMachine[FState][LAction](AChar)
-  //If the that action state is not reachable, let's see if the error action is reachable
-  else if (FStateMachine[FState][TAction.error] <> STATE_ACTION_UNREACHABLE) then
-    FStateMachine[FState][TAction.error](AChar)
-  //No one else, so let's consider it as a char
+  //The indexed array access is much faster than a dictionary
+  if Assigned(FStaticStateMachine[FState][LAction]) then
+    FStaticStateMachine[FState][LAction](AChar)
+  else if Assigned(FStaticStateMachine[FState][TAction.error]) then
+    FStaticStateMachine[FState][TAction.error](AChar)
   else
-    FStateMachine[FState][TAction.char](AChar);
+    FStaticStateMachine[FState][TAction.char](AChar);
+end;
+
+procedure TLexer.Build;
+var
+  LState: TState;
+  LAction: TAction;
+begin
+  for LState := Low(TState) to High(TState) do begin
+    for LAction := Low(TAction) to High(TAction) do
+      FStateMachine.Items[LState].TryGetValue(
+        LAction, FStaticStateMachine[LState][LAction]);
+  end;
 end;
 
 procedure TLexer.Write(const AXmlData: string);
@@ -154,261 +175,266 @@ end;
 
 procedure TLexer.CreateStateMachine;
 var
-  LNoOp: THandler;
-var
-  LState: TState;
-  LAction: TAction;
+  LNoop: TProc<char>;
 begin
-  for LState := Low(TState) to High(TState) do begin
-    for LAction := Low(TAction) to High(TAction) do
-      FStateMachine[LState][LAction] := STATE_ACTION_UNREACHABLE;
-  end;
-
-  LNoOp := procedure(AChar: char)
+  LNoop := procedure(AChar: char)
   begin
     //
   end;
 
   //data state
-  FStateMachine[TState.data][TAction.lt] := procedure(AChar: char)
-  begin
-    if not FData.Trim().IsEmpty() then
-      Emit(TType.text, FData);
-    FTagName := String.Empty;
-    FIsClosing := false;
-    FState := TState.tagBegin;
-  end;
-
-  FStateMachine[TState.data][TAction.char]  := procedure(AChar: char)
-  begin
-    FData := FData + AChar;
-  end;
+  FStateMachine.Add(
+    TState.data, TStateActions.Create([
+      //lt action
+      TActionPair.Create(TAction.lt, procedure(AChar: char) begin
+        if not FData.Trim().IsEmpty() then
+          Emit(TType.text, FData);
+        FTagName := String.Empty;
+        FIsClosing := false;
+        FState := TState.tagBegin;
+      end),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FData := FData + AChar;
+      end)
+    ]));
 
   //cdata state
-  FStateMachine[TState.cdata][TAction.char] := procedure(AChar: char)
-  begin
-    FData := FData + AChar;
-    if FData.EndsWith(']]>') then begin
-      Emit(TType.text, FData.SubString(Length(FData) - 3, 3));
-      FData := String.Empty;
-      FState := TState.data;
-    end;
-  end;
+  FStateMachine.Add(
+    TState.cdata, TStateActions.Create([
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FData := FData + AChar;
+        if FData.EndsWith(']]>') then begin
+          Emit(TType.text, FData.SubString(Length(FData) - 3, 3));
+          FData := String.Empty;
+          FState := TState.data;
+        end;
+      end)
+    ]));
 
   //tagBegin state
-  FStateMachine[TState.tagBegin][TAction.space] := LNoOp;
-
-  FStateMachine[TState.tagBegin][TAction.char] := procedure(AChar: char)
-  begin
-    FTagName := AChar;
-    FState := TState.tagName;
-  end;
-
-  FStateMachine[TState.tagBegin][TAction.slash] := procedure(AChar: char) begin
-    FTagName := String.Empty;
-    FIsClosing := true;
-  end;
+  FStateMachine.Add(
+    TState.tagBegin, TStateActions.Create([
+      //space action
+      TActionPair.Create(TAction.space, LNoop),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FTagName := AChar;
+        FState := TState.tagName;
+      end),
+      //slash action
+      TActionPair.Create(TAction.slash, procedure(AChar: char) begin
+        FTagName := String.Empty;
+        FIsClosing := true;
+      end)
+    ]));
 
   //tagName state
-  FStateMachine[TState.tagName][TAction.space] := procedure(AChar: char)
-  begin
-    if FIsClosing then
-      FState := TState.tagEnd
-    else begin
-      FState := TState.attributeNameStart;
-      Emit(TType.openTag, FTagName);
-    end;
-  end;
+  FStateMachine.Add(
+    TState.tagName, TStateActions.Create([
+      //space action
+      TActionPair.Create(TAction.space, procedure(AChar: char) begin
+        if FIsClosing then
+          FState := TState.tagEnd
+        else begin
+          FState := TState.attributeNameStart;
+          Emit(TType.openTag, FTagName);
+        end;
+      end),
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        if FIsClosing then
+          Emit(TType.closeTag, FTagName)
+        else
+          Emit(TType.openTag, FTagName);
 
-  FStateMachine[TState.tagName][TAction.gt] := procedure(AChar: char)
-  begin
-    if FIsClosing then
-      Emit(TType.closeTag, FTagName)
-    else
-      Emit(TType.openTag, FTagName);
-
-    FData := String.Empty;
-    FState := TState.data;
-  end;
-
-  FStateMachine[TState.tagName][TAction.slash] := procedure(AChar: char)
-  begin
-    FState := TState.tagEnd;
-    Emit(TType.openTag, FTagName);
-  end;
-
-  FStateMachine[TState.tagName][TAction.char] := procedure(AChar: char)
-  begin
-    FTagName := FTagName + AChar;
-    if (FTagName = '![CDATA[') then begin
-      FState := TState.cdata;
-      FData := String.Empty;
-      FTagName := String.Empty;
-    end;
-  end;
+        FData := String.Empty;
+        FState := TState.data;
+      end),
+      //slash action
+      TActionPair.Create(TAction.slash, procedure(AChar: char) begin
+        FState := TState.tagEnd;
+        Emit(TType.openTag, FTagName);
+      end),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FTagName := FTagName + AChar;
+        if (FTagName = '![CDATA[') then begin
+          FState := TState.cdata;
+          FData := String.Empty;
+          FTagName := String.Empty;
+        end;
+      end)
+    ]));
 
   //tagEnd state
-  FStateMachine[TState.tagEnd][TAction.gt] := procedure(AChar: char) begin
-    Emit(TType.closeTag, FTagName);
-    FData := String.Empty;
-    FState := TState.data;
-  end;
-
-  FStateMachine[TState.tagEnd][TAction.char] := LNoOp;
+  FStateMachine.Add(
+    TState.tagEnd, TStateActions.Create([
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        Emit(TType.closeTag, FTagName);
+        FData := String.Empty;
+        FState := TState.data;
+      end),
+      TActionPair.Create(TAction.char, LNoop)
+    ]));
 
   //attributeNameStart state
-  FStateMachine[TState.attributeNameStart][TAction.char] := procedure(AChar: char)
-  begin
-    FAttrName := AChar;
-    FState := TState.attributeName;
-  end;
-
-  FStateMachine[TState.attributeNameStart][TAction.gt] :=  procedure(AChar: char)
-  begin
-    FData := String.Empty;
-    FState := TState.data;
-  end;
-  FStateMachine[TState.attributeNameStart][TAction.space] := LNoOp;
-
-  FStateMachine[TState.attributeNameStart][TAction.slash] := procedure(AChar: char)
-  begin
-    FIsClosing := true;
-    FState := TState.tagEnd;
-  end;
+  FStateMachine.Add(
+    TState.attributeNameStart, TStateActions.Create([
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FAttrName := AChar;
+        FState := TState.attributeName;
+      end),
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        FData := String.Empty;
+        FState := TState.data;
+      end),
+      //space action
+      TActionPair.Create(TAction.space, LNoop),
+      //slash action
+      TActionPair.Create(TAction.slash, procedure(AChar: char) begin
+        FIsClosing := true;
+        FState := TState.tagEnd;
+      end)
+    ]));
 
   //attributeName state
-  FStateMachine[TState.attributeName][TAction.space] := procedure(AChar: char)
-  begin
-    FState := TState.attributeNameEnd;
-  end;
-
-  FStateMachine[TState.attributeName][TAction.equal] := procedure(AChar: char)
-  begin
-    Emit(TType.attributeName, FAttrName);
-    FState := TState.attributeValueBegin;
-  end;
-
-  FStateMachine[TState.attributeName][TAction.gt] := procedure(AChar: char)
-  begin
-    FAttrValue := String.Empty;
-    Emit(TType.attributeName, FAttrName);
-    Emit(TType.attributeValue, FAttrValue);
-    FData := String.Empty;
-    FState := TState.data;
-  end;
-
-  FStateMachine[TState.attributeName][TAction.slash] := procedure(AChar: char)
-  begin
-    FIsClosing := true;
-    FAttrValue := String.Empty;
-    Emit(TType.attributeName, FAttrName);
-    Emit(TType.attributeValue, FAttrValue);
-    FState := TState.tagEnd;
-  end;
-
-  FStateMachine[TState.attributeName][TAction.char] := procedure(AChar: char)
-  begin
-    FAttrName := FAttrName + AChar;
-  end;
+  FStateMachine.Add(
+    TState.attributeName, TStateActions.Create([
+      //space action
+      TActionPair.Create(TAction.space, procedure(AChar: char) begin
+        FState := TState.attributeNameEnd;
+      end),
+      //equal action
+      TActionPair.Create(TAction.equal, procedure(AChar: char) begin
+        Emit(TType.attributeName, FAttrName);
+        FState := TState.attributeValueBegin;
+      end),
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        FAttrValue := String.Empty;
+        Emit(TType.attributeName, FAttrName);
+        Emit(TType.attributeValue, FAttrValue);
+        FData := String.Empty;
+        FState := TState.data;
+      end),
+      //slash action
+      TActionPair.Create(TAction.slash, procedure(AChar: char) begin
+        FIsClosing := true;
+        FAttrValue := String.Empty;
+        Emit(TType.attributeName, FAttrName);
+        Emit(TType.attributeValue, FAttrValue);
+        FState := TState.tagEnd;
+      end),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FAttrName := FAttrName + AChar;
+      end)
+    ]));
 
   //attributeNameEnd state
-  FStateMachine[TState.attributeNameEnd][TAction.space] := LNoOp;
-
-  FStateMachine[TState.attributeNameEnd][TAction.equal] := procedure(AChar: char)
-  begin
-    Emit(TType.attributeName, FAttrName);
-    FState := TState.attributeValueBegin;
-  end;
-
-  FStateMachine[TState.attributeNameEnd][TAction.gt] := procedure(AChar: char)
-  begin
-    FAttrValue := String.Empty;
-    Emit(TType.attributeName, FAttrName);
-    Emit(TType.attributeValue, FAttrValue);
-    FData := String.Empty;
-    FState := TState.data;
-  end;
-
-  FStateMachine[TState.attributeNameEnd][TAction.char] := procedure(AChar: char)
-  begin
-    FAttrValue := String.Empty;
-    Emit(TType.attributeName, FAttrName);
-    Emit(TType.attributeValue, FAttrValue);
-    FAttrName := AChar;
-    FState := TState.attributeName;
-  end;
+  FStateMachine.Add(
+    TState.attributeNameEnd, TStateActions.Create([
+      //space action
+      TActionPair.Create(TAction.space, LNoop),
+      //equal action
+      TActionPair.Create(TAction.equal, procedure(AChar: char) begin
+        Emit(TType.attributeName, FAttrName);
+        FState := TState.attributeValueBegin;
+      end),
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        FAttrValue := String.Empty;
+        Emit(TType.attributeName, FAttrName);
+        Emit(TType.attributeValue, FAttrValue);
+        FData := String.Empty;
+        FState := TState.data;
+      end),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FAttrValue := String.Empty;
+        Emit(TType.attributeName, FAttrName);
+        Emit(TType.attributeValue, FAttrValue);
+        FAttrName := AChar;
+        FState := TState.attributeName;
+      end)
+    ]));
 
   //attributeValueBegin state
-  FStateMachine[TState.attributeValueBegin][TAction.space] := LNoop;
-
-  FStateMachine[TState.attributeValueBegin][TAction.quote] := procedure(AChar: char)
-  begin
-    FOpeningQuote := AChar;
-    FAttrValue := String.Empty;
-    FState := TState.attributeValue;
-  end;
-
-  FStateMachine[TState.attributeValueBegin][TAction.gt] := procedure(AChar: char)
-  begin
-    FAttrValue := String.Empty;
-    Emit(TType.attributeValue, FAttrValue);
-    FData := String.Empty;
-    FState := TState.data;
-  end;
-
-  FStateMachine[TState.attributeValueBegin][TAction.char] := procedure(AChar: char)
-  begin
-    FOpeningQuote := String.Empty;
-    FAttrValue := AChar;
-    FState := TState.attributeValue;
-  end;
+  FStateMachine.Add(
+    TState.attributeValueBegin, TStateActions.Create([
+      //space action
+      TActionPair.Create(TAction.space, LNoop),
+      //quote action
+      TActionPair.Create(TAction.quote, procedure(AChar: char) begin
+        FOpeningQuote := AChar;
+        FAttrValue := String.Empty;
+        FState := TState.attributeValue;
+      end),
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        FAttrValue := String.Empty;
+        Emit(TType.attributeValue, FAttrValue);
+        FData := String.Empty;
+        FState := TState.data;
+      end),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FOpeningQuote := String.Empty;
+        FAttrValue := AChar;
+        FState := TState.attributeValue;
+      end)
+    ]));
 
   //attributeValue state
-  FStateMachine[TState.attributeValue][TAction.space] := procedure(AChar: char)
-  begin
-    if not FOpeningQuote.IsEmpty() then
-      FAttrValue := FAttrValue + AChar
-    else begin
-      Emit(TType.attributeValue, FAttrValue);
-      FState := TState.attributeNameStart;
-    end;
-  end;
-
-  FStateMachine[TState.attributeValue][TAction.quote] := procedure(AChar: char)
-  begin
-    if (FOpeningQuote = AChar) then begin
-      Emit(TType.attributeValue, FAttrValue);
-      FState := TState.attributeNameStart;
-    end else
-      FAttrValue := FAttrValue + AChar;
-  end;
-
-  FStateMachine[TState.attributeValue][TAction.gt] := procedure(AChar: char)
-  begin
-    if not FOpeningQuote.IsEmpty() then
-      FAttrValue := FAttrValue + AChar
-    else begin
-      Emit(TType.attributeValue, FAttrValue);
-      FData := String.Empty;
-      FState := TState.data;
-    end;
-  end;
-
-  FStateMachine[TState.attributeValue][TAction.slash] := procedure(AChar: char)
-  begin
-    if not FOpeningQuote.IsEmpty() then
-      FAttrValue := FAttrValue + AChar
-    else begin
-      Emit(TType.attributeValue, FAttrValue);
-      FIsClosing := true;
-      FState := TState.tagEnd;
-    end;
-  end;
-
-  FStateMachine[TState.attributeValue][TAction.char] := procedure(AChar: char)
-  begin
-    FAttrValue := FAttrValue + AChar;
-  end;
+  FStateMachine.Add(
+    TState.attributeValue, TStateActions.Create([
+      //space action
+      TActionPair.Create(TAction.space, procedure(AChar: char) begin
+        if not FOpeningQuote.IsEmpty() then
+          FAttrValue := FAttrValue + AChar
+        else begin
+          Emit(TType.attributeValue, FAttrValue);
+          FState := TState.attributeNameStart;
+        end;
+      end),
+      //quote action
+      TActionPair.Create(TAction.quote, procedure(AChar: char) begin
+        if (FOpeningQuote = AChar) then begin
+          Emit(TType.attributeValue, FAttrValue);
+          FState := TState.attributeNameStart;
+        end else
+          FAttrValue := FAttrValue + AChar;
+      end),
+      //gt action
+      TActionPair.Create(TAction.gt, procedure(AChar: char) begin
+        if not FOpeningQuote.IsEmpty() then
+          FAttrValue := FAttrValue + AChar
+        else begin
+          Emit(TType.attributeValue, FAttrValue);
+          FData := String.Empty;
+          FState := TState.data;
+        end;
+      end),
+      //slash action
+      TActionPair.Create(TAction.slash, procedure(AChar: char) begin
+        if not FOpeningQuote.IsEmpty() then
+          FAttrValue := FAttrValue + AChar
+        else begin
+          Emit(TType.attributeValue, FAttrValue);
+          FIsClosing := true;
+          FState := TState.tagEnd;
+        end;
+      end),
+      //char action
+      TActionPair.Create(TAction.char, procedure(AChar: char) begin
+        FAttrValue := FAttrValue + AChar;
+      end)
+    ]));
 end;
 
 end.
